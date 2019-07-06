08b01b8dc689be0671631b5895c2ca77b815c01b
Fredrik Meyer <hrmeyer@gmail.com>
2019-07-06 20:44:32 +0200

    Remove traces of unused port

--- src/static/index.js
+++ src/static/index.js
@@ -6,10 +6,6 @@ var app = Elm.Main.init({
     node: document.getElementById("app")
 });
 
-window.onblur = function () {
-    app.ports.blurs.send()
-}
-
 
 document.body.addEventListener('keydown', e => {
     if (e.metaKey && e.keyCode == 13) {
